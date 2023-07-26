38085545d522196aca37f080d28db412be87e102
ambientWave <ambientWave@online.dev>
2023-07-26 11:13:22 +0400

    stillBroken

--- custom/custom_restaurant/static/src/js/HideEditButtonInFormViewBasedOnACondition.js
+++ custom/custom_restaurant/static/src/js/HideEditButtonInFormViewBasedOnACondition.js
@@ -1,5 +1,7 @@
 odoo.define('custom_restaurant.HideEditButtonBasedOnACondition', function (require) {
     "use strict";
+
+    // var session = require('web.session');
     var FormController = require('web.FormController');
 
     FormController.include({
@@ -9,11 +11,16 @@ odoo.define('custom_restaurant.HideEditButtonBasedOnACondition', function (requi
             var button = this.$buttons.find("o_form_button_edit").show();
             if (this.modelName === "stock.picking") {
                 var currentModelViewedRecord = this.model.get(this.handle).data;
+                console.log(this.modelName);
+                console.log(this.model.get(this.handle).data);
+                console.log(currentModelViewedRecord.state);
 
                 if (currentModelViewedRecord.state === 'done') {
-                    this.$buttons.find("o_form_button_edit").show().hide;
+                    this.$buttons.find("o_form_button_edit").hide();
                 }
             }
+
+            return this._super.apply(this, arguments);
 
         }
     //     init: function(ViewInfo, params) {
